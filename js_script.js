f4a17085b5b8aedec78a5c33cb01591dcb969647
ThereseXD <35051370+ThereseXD@users.noreply.github.com>
2024-07-29 16:52:28 -0400

    Structure02 without extra credits

--- js_script.js
+++ js_script.js
@@ -1,36 +1,102 @@
-let num1 = 0;
-let num2 = 0;
-let operator;
+let num1 = '0';
+let num2 = '0';
+let operator=''; 
+let output;
 
 let input = document.querySelectorAll('button');
-let display = document.querySelector('.display');
+let inputDisplay = document.querySelector('.inputDisplay');
+let outcomeDisplay = document.querySelector('.outcomeDisplay');
 
 input.forEach(button => {
     button.addEventListener('click',(event)=> {
-        let collectInput = event.target.textContent;
-        console.log(collectInput);
-        display.textContent = collectInput;
+        let inputEle = event.target
+        if(inputEle.classList.contains('digit') && (!operator)){
+            if(num1[0]== 0){
+                num1 = inputEle.textContent;
+                outcomeDisplay.textContent = num1;
+            } else {
+                num1 += inputEle.textContent;
+                outcomeDisplay.textContent = num1;
+            }
+            console.log(num1)
+        } else if (inputEle.classList.contains('operator') && (!operator)){
+            
+            operator = inputEle.textContent;
+            inputDisplay.textContent = num1 + operator;
+            outcomeDisplay.textContent = num2;
+            console.log(num1)
+            console.log(operator)
+        } else if(inputEle.classList.contains('digit') && operator){
+            if(num2[0]== 0){
+                num2 = inputEle.textContent;
+                outcomeDisplay.textContent = num2;
+            } else {
+                num2 += inputEle.textContent;
+                outcomeDisplay.textContent = num2;
+            }
+            console.log(num2)
+        } else if (inputEle.classList.contains('equal')){
+            if((!num1)&& (!num2)&&(!operator)){
+                inputDisplay.textContent = '';
+                outcomeDisplay.textContent = '0';
+            } else if((!operator) || (!num2)){
+                inputDisplay.textContent = ''
+                outcomeDisplay.textContent = num1;
+            } else if(operator === '/' && Number(num2)=== 0){
+                alert(`Error!Can't be divided by 0!`);
+                
+            } else {output = operate(num1,num2,operator);
+                inputDisplay.textContent = num1 + operator + num2 + '=';
+                outcomeDisplay.textContent = outputNum(output);
+                
+                console.log(outputNum(output))
+        }
+        } else if (inputEle.classList.contains('operator') && operator){
+            if(num2 === '0'){
+                operator = inputEle.textContent;
+                inputDisplay.textContent = num1 + operator;
+                outcomeDisplay.textContent = num2;
+            } else if(operator === '/' && Number(num2)=== 0){
+                alert(`Error!Can't be divided by 0!`);
+            } else {
+                output = operate(num1,num2,operator);
+                num1 = outputNum(output);
+                num2 = '0';
+                operator = inputEle.textContent;
+                inputDisplay.textContent = num1 + operator
+                outcomeDisplay.textContent = num2;
+            }
+        } else if (inputEle.classList.contains('clear')){
+            num1 = '';
+            num2 = '';
+            operator = '';
+            outcomeDisplay.textContent = '0';
+            inputDisplay.textContent = '';
+        }
+       
 
     });
 });
 
 
 
+
 function add(num1,num2){
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1,num2){
-    return num1-num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1,num2){
-    return num1 * num2 ;
+    return Number(num1) * Number(num2) ;
 }
 
 function divide(num1,num2){
-    return num2 === 0? 'Error' : num1 / num2;
+    return Number(num1)/Number(num2);
 }
+
 
 function operate(num1,num2,operator){
     switch(operator){
@@ -51,3 +117,14 @@ function operate(num1,num2,operator){
             break;
     }
 }
+
+function outputNum(num){
+    let decimalNums = num.toString().split('.')
+
+    if (decimalNums[1]!==undefined){
+        return decimalNums[1].length > 5 ? num.toFixed(5) : num
+    }
+   
+    return num;
+}
+
